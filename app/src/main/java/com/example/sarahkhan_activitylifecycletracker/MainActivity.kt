5b7ff85509f89a663aa697a4b7aca36022ed7951
Sarah Khan <sarkhan@bu.edu>
2024-02-11 19:15:21 -0500

    added lifecycle events and log prints

--- app/src/main/java/com/example/sarahkhan_activitylifecycletracker/MainActivity.kt
+++ app/src/main/java/com/example/sarahkhan_activitylifecycletracker/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.sarahkhan_activitylifecycletracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.EditText
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
